c1c8dc2f85ecebe14729de7045b9ff86d9c4ad28
Katsiaryna Fedarovich <kathefy215@gmail.com>
2019-09-22 18:00:26 +0200

    Promises

---
+++ exercises/promises.js
@@ -0,0 +1,19 @@
+function randomTrueOrFalse() {
+    return Math.random() < 0.5;
+}
+
+function fetchSomeData() {
+    return new Promise((resolve, reject) => {
+        if(randomTrueOrFalse()) {
+            resolve(['a', 'b', 'c']);
+        } else {
+            reject(new Error('Error description'));
+        }
+    });
+}
+
+fetchSomeData()
+    .then(
+        data => {console.log('success', data)},
+        error => { console.log('error', error) }
+    );
