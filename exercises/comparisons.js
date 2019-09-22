b1d09db205335463e1c2d6430d47efbb16ad77ad
Katsiaryna Fedarovich <kathefy215@gmail.com>
2019-09-22 15:01:17 +0200

    Comparisons

---
+++ exercises/comparisons.js
@@ -0,0 +1,17 @@
+// Proste
+console.log('5 === 5', 5 === 5);
+console.log('true === true', true === true);
+console.log('false === false', false === false);
+console.log('"abc" === "abc"', "abc" === "abc");
+console.log('undefined === undefined', undefined === undefined);
+
+// Objekty
+console.log('{} === {}', {} === {});
+console.log('{foo: "bar"} === {foo: "bar"}', {foo: "bar"} === {foo: "bar"});
+
+const a = { a: 'A'};
+const b = a;
+console.log('a === b', a === b);
+
+
+
